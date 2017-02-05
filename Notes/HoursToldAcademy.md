f5f3ad9cac663a21f22f4588c55836a69fe7c6cb
Rick Love <rick@toldpro.com>
2017-02-05 00:48:27 -0600

    Create Azure Function App

--- Notes/HoursToldAcademy.md
+++ Notes/HoursToldAcademy.md
@@ -7,4 +7,18 @@
 ## 21:46-24:00
 ## 00:00-0:41
 
-- Show Images
+- START Word To Picture
+
+## 0:42-0:50
+
+- Create Azure Function App
+
+# Tasks
+
+## Word to Picture
+
+- Get Word List
+- Save Selection
+- Get Popular Selections (Correct Answers)
+- Add Next Button
+- Add Flag Button
--- src-server/default.ts
+++ src-server/default.ts
@@ -1,6 +1,7 @@
 import { main as main_inner } from '@told/azure-functions-server/lib/src-server/resource';
 
 export async function main(context: any, request: any) {
-    request.query.name = 'index.html';
+    // request.query.name = 'index.html';
+    request.query.name = 'word-to-picture.html';
     return await main_inner(context, request, 0);
 }
